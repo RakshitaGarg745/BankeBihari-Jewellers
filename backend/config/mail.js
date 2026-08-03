9b791639749856c47e161881dd42b763388eb230
Rakshita Garg <rakshitagarg@Macbook-3.local>
2026-08-04 00:11:18 +0530

    Replace Gmail SMTP with Brevo

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -1,21 +1,14 @@
 const nodemailer = require("nodemailer");
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
 
 const transporter = nodemailer.createTransport({
-
     host: "smtp-relay.brevo.com",
-
-    port: 587,
-
-    secure: false,
-
+    port: 465,
+    secure: true,
     auth: {
-
         user: process.env.EMAIL_USER,
-
         pass: process.env.EMAIL_PASS
-
     }
-
 });
 
 module.exports = transporter;
