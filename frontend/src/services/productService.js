bdc29cc7720ad87e59569c20180565a7ceb515d5
Rakshita Garg <rakshitagarg@Macbook-3.local>
2026-08-03 10:15:11 +0530

    Remove remaining localhost references

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,29 +1,17 @@
 import axios from "axios";
 
-const API = "http://localhost:3001/products";
+const API = `${process.env.REACT_APP_API_URL}/products`;
 
 export const getProducts = () => axios.get(API);
 
 export const getProduct = (id) => axios.get(`${API}/${id}`);
 
 export const getProductsByCategory = async (category) => {
-
-    const response = await axios.get(
-        `http://localhost:3001/products/category/${category}`
-    );
-
+    const response = await axios.get(`${API}/category/${category}`);
     return response.data;
-
 };
 
 export const getProductById = async (id) => {
-
-    const res = await axios.get(
-
-        `http://localhost:3001/products/${id}`
-
-    );
-
-    return res.data;
-
+    const response = await axios.get(`${API}/${id}`);
+    return response.data;
 };
